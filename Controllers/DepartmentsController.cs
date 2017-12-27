d231d9850ac699d0bc16acba7869e08a600b65e7
kubapyda <kubapyda95@gmail.com>
2017-12-27 17:03:11 +0100

    Dodanie endpointa "Kierunkii studiów"

---
+++ Controllers/DepartmentsController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using StudentsJournalWeb.Models;
+
+namespace StudentsJournalWeb.Controllers
+{
+    public class DepartmentsController : ApiController
+    {
+        private JournalWebEntities db = new JournalWebEntities();
+
+        // GET: api/Departments
+        public IQueryable GetDepartment()
+        {
+            var departments = from d in db.Department
+                              select new
+                              {
+                                  id = (int)d.department_id,
+                                  name = d.department_name,
+                                  direction = d.Direction.direction_name
+                              };
+            return departments;
+        }
+
+        // GET: api/Departments/5
+        [ResponseType(typeof(Department))]
+        public IHttpActionResult GetDepartment(decimal id)
+        {
+            Department department = db.Department.Find(id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(department);
+        }
+
+        // PUT: api/Departments/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutDepartment(decimal id, Department department)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != department.department_id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(department).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DepartmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Departments
+        [ResponseType(typeof(Department))]
+        public IHttpActionResult PostDepartment(Department department)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Department.Add(department);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = department.department_id }, department);
+        }
+
+        // DELETE: api/Departments/5
+        [ResponseType(typeof(Department))]
+        public IHttpActionResult DeleteDepartment(decimal id)
+        {
+            Department department = db.Department.Find(id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            db.Department.Remove(department);
+            db.SaveChanges();
+
+            return Ok(department);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool DepartmentExists(decimal id)
+        {
+            return db.Department.Count(e => e.department_id == id) > 0;
+        }
+    }
+}
