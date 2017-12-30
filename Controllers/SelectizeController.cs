8a4e2a01df7b169edfb992a123a6608af8d29118
kubapyda <kubapyda95@gmail.com>
2017-12-30 15:10:45 +0100

    Dodanie endpointa "Przedmioty"

---
+++ Controllers/SelectizeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using StudentsJournalWeb.Models;
+
+namespace StudentsJournalWeb.Controllers
+{
+    public class SelectizeController : ApiController
+    {
+        private JournalWebEntities db = new JournalWebEntities();
+
+        // GET: api/Selectize
+        public IQueryable GetLeaders()
+        {
+            var selectize = from d in db.Leaders
+                            select new
+                            {
+                                id = (int)d.lead_ID,
+                                name = d.lead_name + " " + d.lead_surname
+                            };
+            return selectize;
+        }
+    }
+}
---
+++ Controllers/SubjectsController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using StudentsJournalWeb.Models;
+
+namespace StudentsJournalWeb.Controllers
+{
+    public class SubjectsController : ApiController
+    {
+        private JournalWebEntities db = new JournalWebEntities();
+
+        // GET: api/Subjects
+        public IQueryable GetSubjects()
+        {
+            var subjects = from d in db.Subjects
+                           select new
+                           {
+                               id = (int)d.subject_ID,
+                               name = d.subject_name,
+                               leader = d.Leaders.lead_name + " " + d.Leaders.lead_surname
+                           };
+            return subjects;
+        }
+
+        // GET: api/Subjects/5
+        [ResponseType(typeof(Subjects))]
+        public IHttpActionResult GetSubjects(decimal id)
+        {
+            Subjects subjects = db.Subjects.Find(id);
+            if (subjects == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(subjects);
+        }
+
+        // PUT: api/Subjects/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutSubjects(decimal id, Subjects subjects)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != subjects.subject_ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(subjects).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SubjectsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Subjects
+        [ResponseType(typeof(Subjects))]
+        public IHttpActionResult PostSubjects(Subjects subjects)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Subjects.Add(subjects);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = subjects.subject_ID }, subjects);
+        }
+
+        // DELETE: api/Subjects/5
+        [ResponseType(typeof(Subjects))]
+        public IHttpActionResult DeleteSubjects(decimal id)
+        {
+            Subjects subjects = db.Subjects.Find(id);
+            if (subjects == null)
+            {
+                return NotFound();
+            }
+
+            db.Subjects.Remove(subjects);
+            db.SaveChanges();
+
+            return Ok(subjects);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool SubjectsExists(decimal id)
+        {
+            return db.Subjects.Count(e => e.subject_ID == id) > 0;
+        }
+    }
+}
